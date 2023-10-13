6bd99fe01a4d512d9ab5b64ac748aebcbae0e2b3
dvparikh <dvparikh@COE-CS-pc17.OU.AD3.UCDAVIS.EDU>
2023-10-13 15:32:16 -0700

    Done!

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -2,67 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-//! IS FAULTY ON FILE INPUTS
-
-int findword(char *buff, char *word) {
-    if (!buff || !word) {
-        return -1;
-    }
-
-    size_t size;
-    if((size = strlen(word)) == 0) {
-        return 0;
-    }
-    size -= 1;
-
-    int b_idx = 0;
-    int w_idx = 0;
-    char c;
-    while ((c = buff[b_idx++]) != '\0') {
-        if (w_idx == size) {
-            return 1;
-        }
-        if (c == word[w_idx]) {
-            w_idx++;
-        } else {
-            w_idx = 0;
-        }
-    }
-    return 0;
-}
-
-void wgrep(char *word, FILE *fp) {
-    char *line = NULL;
-    size_t len = 0;
-    while (getline(&line, &len, fp) != -1) {
-        if (findword(line, word) > 0) {
-            printf("%s", line);
-        }
-    }
-    free(line);
-}
 
 int main(int argc, char **argv) {
-    if (argc == 1) {
-        printf("wgrep: searchterm [file...]\n");
-        return 1;
-    }
+  FILE *fp;
+  char *buffer = NULL;
+  size_t buffer_size = 32;
 
-    if (argc == 2) {
-        wgrep(argv[1], stdin);
-        return 0;
-    }
-    
-    FILE *fp;
-    for (int i = 2; i < argc; i++) {
-        fp = fopen(argv[i], "r");
-        if (!fp) {
-            printf("wgrep: cannot open file %s", argv[i]);
-            return 1;
-        }
-        wgrep(argv[1], fp);
-        fclose(fp);
-    }
+  if (argc <= 1) {
+    printf("wgrep: searchterm [file ...]\n");
+    exit(EXIT_FAILURE);
+  }
 
-    return 0;
+  char *search_term = argv[1];
+  char *target_file = argv[2];
+
+  if (target_file)
+    fp = fopen(target_file, "r");
+  else
+    fp = stdin;
+
+
+  if (fp == NULL) {
+    printf("wgrep: cannot open file\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (getline(&buffer, &buffer_size, fp) != -1)
+    if (strstr(buffer, search_term) )
+      printf("%s", buffer);
+
+  fclose(fp);
+  free(buffer);
+  exit(EXIT_SUCCESS);
 }
--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -1,97 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+#include <string.h>
 
-/***************************** Binary Converters *****************************/
+int main(int argc, char *argv[]) {
+  if (argc <= 1) {
+    printf("wzip: file1 [file2 ...]\n");
+    exit(EXIT_FAILURE);
+  }
 
-#define BINARY_BYTE "%c%c%c%c%c%c%c%c"
-#define PRINTF_BYTE_TO_BINARY(i)    \
-    (((i) & 0x80) ? '1' : '0'), \
-    (((i) & 0x40) ? '1' : '0'), \
-    (((i) & 0x20) ? '1' : '0'), \
-    (((i) & 0x10) ? '1' : '0'), \
-    (((i) & 0x08) ? '1' : '0'), \
-    (((i) & 0x04) ? '1' : '0'), \
-    (((i) & 0x02) ? '1' : '0'), \
-    (((i) & 0x01) ? '1' : '0')
+  FILE *file;
+  char character[2] = "";
+  char previous_character[2] = "";
+  int char_occurrences = 0;
 
-#define BINARY_2_BYTE \
-    BINARY_BYTE     BINARY_BYTE
-#define PRINTF_2BYTE_TO_BINARY(i) \
-    PRINTF_BYTE_TO_BINARY((i) >> 8),   PRINTF_BYTE_TO_BINARY(i)
+  for (int i=1; i < argc; i++) {
+    char *file_name = argv[i];
+    if ((file = fopen(file_name, "r")) == NULL) {
+      perror("wzip: cannot open file");
+      exit(EXIT_FAILURE);
+    }
 
-#define BINARY_4_BYTE \
-    BINARY_2_BYTE   BINARY_2_BYTE
-#define PRINTF_4BYTE_TO_BINARY(i) \
-    PRINTF_2BYTE_TO_BINARY((i) >> 16), PRINTF_2BYTE_TO_BINARY(i)
-
-/******************************** Main Program *******************************/
-
-void unzip(FILE* fp) {
-    char *line = NULL;
-    size_t len;
-
-    char c;
-    int i, n;
-    uint32_t l;
-
-    while(getline(&line, &len, fp) != -1) {
-        while (i < len) {
-            n = i + 32;
-            for (l = 0; i < n; i++) {
-                l = 2 * l + (line[i] - '0');
-            }
-            c = line[i++];
-            while (l--) {
-                printf("%c", c);
-            }
+    while (fread(&character, 1, 1, file)) {
+      if (strcmp(character, previous_character) == 0) {
+        char_occurrences++;
+      } else {
+        if (previous_character[0] != '\0') {
+          fwrite(&char_occurrences, 4, 1, stdout);
+          fwrite(previous_character, 1, 1, stdout);
         }
+        char_occurrences = 1;
+        strcpy(previous_character, character);
+      }
     }
+    fclose(file);
+  }
 
-    free(line);
-}
-
-#define write(l,c) { \
-    fwrite(&l, sizeof(l), 1, stdout); \
-    printf("%c", c); \
-}
-
-void zip(FILE *fp) {
-    char c, new; 
-    uint32_t l = 0;
-
-    new = c = fgetc(fp);
-    do {
-        if (new == c) {
-            ++l;
-        } else {
-            write(l, c);
-            c = new;
-            l = 1;
-        }
-    } while ((new = fgetc(fp)) != EOF);
-    if (c != '\n') {
-        write(l, c);
-    }
-}
-
-int main(int argc, char **argv) {
-    if (argc == 1) {
-        printf("wzip: file1 [file2 ...]\n");
-        return 1;
-    }
-
-    FILE *fp;
-    
-    for (int i = 1; i < argc; i++) {
-        fp = fopen(argv[i], "r");
-        if (!fp) {
-            printf("wzip: invalid file %s", argv[i]);
-        }
-        zip(fp);
-        fclose(fp);
-    }
-
-    printf("\n"); 
-    return 0;
+  fwrite(&char_occurrences, 4, 1, stdout);
+  fwrite(previous_character, 1, 1, stdout);
 }
